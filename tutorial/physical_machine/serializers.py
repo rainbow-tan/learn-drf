95659c012c3ca26b3759908d6ef2e71017b37d5a
tanxiaohui <xiaohui.tan@greatdb.com>
2023-05-15 10:16:01 +0800

    注释

--- tutorial/physical_machine/serializers.py
+++ tutorial/physical_machine/serializers.py
@@ -9,9 +9,7 @@ class MachineSerializer(serializers.Serializer):
         pass
 
     ip = serializers.CharField()
-    # host = serializers.CharField(source='ip')
+    # host = serializers.CharField(source='ip')  # 实现更改key的方式 通过指定 source字段 则key ip就变成了key host了
     core = serializers.IntegerField()
     memory = serializers.CharField()
     disk = serializers.CharField()
-
-
